d5cfdeb92433b95d98f06ab57b41233ff8cba4cf
BrunoBmassa <brunobmassaneiro@gmail.com>
2024-12-07 15:42:04 -0300

    Add files via upload

---
+++ desafio2.py
@@ -0,0 +1,21 @@
+
+# Desafio com funções
+'''
+Criar um programa que calcula a quantidade de tinta necessária para pintar uma parede. 
+O usuário deverá fornecedor as seguintes informações: Rendimento, altura e largura.
+O programa deverá mostrar na tela a mensagem ' Você necessita de x latas de tinta'
+'''
+
+rendimento= int(input('Qual é o rendimento da lata? '))
+altura= int(input('Qual é a altura da parede? '))
+largura= int(input('Qual é a largura da parede? '))
+
+def calculo_tinta():
+    total = (altura * largura) / rendimento
+    print(f'Você precisa de {total}latas de tinta')
+    
+
+calculo_tinta()
+ 
+
+
